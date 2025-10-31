5d40d34d41ec1cd7e0869e63f9378db8a98133d3
Ai-Preneur <ai-preneur@outlook.com>
2025-11-01 05:22:17 +0530

    Update server.js to revert CORS edit

--- server.js
+++ server.js
@@ -16,15 +16,16 @@ const allowedOrigins = [
   'https://vaasal-kadhavu.vercel.app'
 ];
 
-app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-}));
+// app.use(cors({
+//   origin: function(origin, callback) {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   }
+// }));
+app.use(cors());
 app.use(express.json());
 
 // Middleware to verify JWT token
@@ -80,3 +81,4 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 
 });
+
